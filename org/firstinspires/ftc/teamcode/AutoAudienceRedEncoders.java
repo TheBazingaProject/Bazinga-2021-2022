ce863a05a9d95ebecee81c0d2734c31871545f3f
Hackyzz <91227333+Hackyzz@users.noreply.github.com>
2021-11-19 19:30:33 -0600

    Update AutoAudienceRedEncoders.java

--- org/firstinspires/ftc/teamcode/AutoAudienceRedEncoders.java
+++ org/firstinspires/ftc/teamcode/AutoAudienceRedEncoders.java
@@ -37,7 +37,9 @@ public class AutoAudienceRedEncoders extends OpMode {
 
     // Declare OpMode members.
     Hardwaremap robot = new Hardwaremap();
-    private ElapsedTime runtime = new ElapsedTime();
+    ElapsedTime runtime = new ElapsedTime();
+    Movement movement = new Movement();
+
 
     String task = "Start";
 
@@ -151,7 +153,7 @@ public class AutoAudienceRedEncoders extends OpMode {
      */
     @Override
     public void start() {
-        resetEncoder();
+        movement.resetEncoder();
     }
 
     /*
@@ -178,19 +180,19 @@ public class AutoAudienceRedEncoders extends OpMode {
 //                break;
 
             case "strafe to carousel":
-                encoderDrive(TURN_SPEED, -30, 30);
+                movement.encoderDrive(TURN_SPEED, -30, 30);
                 task = "stop";
                 break;
 
 //            case "move to carousel":
-//                encoderDrive(DRIVE_SPEED, -30, -30);
-//                resetAngle();
+//                movement.encoderDrive(DRIVE_SPEED, -30, -30);
+//                movement.resetAngle();
 //                task = "spin carousel";
 //                break;
 ////
 //            case "move to carousel2":
-//                if (checkEncoderDone()) {
-//                    encoderComplete();
+//                if (movement.checkEncoderDone()) {
+//                    movement.encoderComplete();
 //                    runtime.reset();
 //                    task = "spin carousel";
 //                }
@@ -206,8 +208,8 @@ public class AutoAudienceRedEncoders extends OpMode {
 //                break;
 
             case "spin carousel":
-                if (checkEncoderDone()) {
-                    encoderComplete();
+                if (movement.checkEncoderDone()) {
+                    movement.encoderComplete();
                     runtime.reset();
                     robot.spinner.setPower(0.5);
                     if (runtime.seconds() > 1.5) {
@@ -218,18 +220,18 @@ public class AutoAudienceRedEncoders extends OpMode {
                 }
 
             case "shift to hub":
-                encoderStrafe(DRIVE_SPEED, 6, 0);
-                if (checkEncoderDone()){
-                    encoderComplete();
+                movement.encoderStrafe(DRIVE_SPEED, 6, 0);
+                if (movement.checkEncoderDone()){
+                    movement.encoderComplete();
                     runtime.reset();
                     task = "move to hub";
                 }
                 break;
 
             case "move to hub":
-                encoderDrive(DRIVE_SPEED,4, 4);
-                if (checkEncoderDone()) {
-                    drive(0, 0);
+                movement.encoderDrive(DRIVE_SPEED,4, 4);
+                if (movement.checkEncoderDone()) {
+                    movement.drive(0, 0);
                     task = "turn to hub";
                     runtime.reset();
                 }
@@ -242,9 +244,9 @@ public class AutoAudienceRedEncoders extends OpMode {
 //                break;
 
             case "turn to hub":
-                encoderDrive(TURN_SPEED,-4, 4);
-                if (checkEncoderDone() == true) {
-                    encoderComplete();
+                movement.encoderDrive(TURN_SPEED,-4, 4);
+                if (movement.checkEncoderDone() == true) {
+                    movement.encoderComplete();
                     runtime.reset();
                     task = "park";
                 }
@@ -260,27 +262,27 @@ public class AutoAudienceRedEncoders extends OpMode {
                 break;
 
             case "forward":
-                encoderDrive(DRIVE_SPEED, 5, 5);
-                if (checkEncoderDone() == true){
-                    encoderComplete();
+                movement.encoderDrive(DRIVE_SPEED, 5, 5);
+                if (movement.checkEncoderDone() == true){
+                    movement.encoderComplete();
                     runtime.reset();
                     task = "strafe to wall";
                 }
                 break;
 
             case "strafe to wall":
-                encoderStrafe(DRIVE_SPEED, -12, 12);
-                if (checkEncoderDone() == true){
-                    encoderComplete();
+                movement.encoderStrafe(DRIVE_SPEED, -12, 12);
+                if (movement.checkEncoderDone() == true){
+                    movement.encoderComplete();
                     runtime.reset();
                     task = "back into square";
                 }
                 break;
 
             case "back into square":
-                encoderDrive(DRIVE_SPEED, -3.5, -3.5);
-                if (checkEncoderDone() == true) {
-                    encoderComplete();
+                movement.encoderDrive(DRIVE_SPEED, -3.5, -3.5);
+                if (movement.checkEncoderDone() == true) {
+                    movement.encoderComplete();
                     runtime.reset();
                     task = "stop";
                 }
@@ -297,7 +299,7 @@ public class AutoAudienceRedEncoders extends OpMode {
 
         telemetry.addData("task: ", task);
         telemetry.addData("sec:  ", runtime.seconds());
-        telemetry.addData("angle", getAngle());
+        telemetry.addData("angle", movement.getAngle());
         telemetry.addLine("fleft   fright   bright   bleft");
         telemetry.addData("Target",  "Running to %7d :%7d :%7d :%7d", robot.fleft.getTargetPosition(), robot.fright.getTargetPosition(), robot.bright.getTargetPosition(), robot.bleft.getTargetPosition());
         telemetry.addData("Current",  "Running at %7d :%7d :%7d :%7d", robot.fleft.getCurrentPosition(), robot.fright.getCurrentPosition(), robot.bright.getCurrentPosition(), robot.bleft.getCurrentPosition());
@@ -311,203 +313,6 @@ public class AutoAudienceRedEncoders extends OpMode {
     public void stop() {
     }
 
-    public void encoderDrive(double speed, double rightInches, double leftInches) {
-        int frightTarget;
-        int fleftTarget;
-        int brightTarget;
-        int bleftTarget;
-
-
-        // Determine new target position, and pass to motor controller
-        fleftTarget = robot.fleft.getCurrentPosition() - (int)(leftInches * COUNTS_PER_INCH);
-        frightTarget = robot.fright.getCurrentPosition() - (int)(rightInches * COUNTS_PER_INCH);
-        bleftTarget = robot.bleft.getCurrentPosition() - (int)(leftInches * COUNTS_PER_INCH);
-        brightTarget = robot.bright.getCurrentPosition() - (int)(rightInches * COUNTS_PER_INCH);
-        robot.fright.setTargetPosition(frightTarget);
-        robot.fleft.setTargetPosition(fleftTarget);
-        robot.bright.setTargetPosition(brightTarget);
-        robot.bleft.setTargetPosition(bleftTarget);
-
-        // Turn On RUN_TO_POSITION
-        robot.fright.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        robot.fleft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        robot.bright.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        robot.bleft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        // reset the timeout time and start motion.
-        //runtime.reset();
-        robot.fright.setPower(Math.abs(speed));
-        robot.fleft.setPower(Math.abs(speed));
-        robot.bright.setPower(Math.abs(speed));
-        robot.bleft.setPower(Math.abs(speed));
-    }
-
-    // frightBleft is positive and fleftBright is negative to strafe left
-    public void encoderStrafe(double speed,
-                              double frightBleftInches, double fleftBrightInches) {
-        int newFleftTarget;
-        int newFrightTarget;
-        int newBleftTarget;
-        int newBrightTarget;
-
-        // Determine new target position, and pass to motor controller
-        newFleftTarget = robot.fleft.getCurrentPosition() + (int)(fleftBrightInches * COUNTS_PER_INCH);
-        newFrightTarget = robot.fright.getCurrentPosition() + (int)(frightBleftInches * COUNTS_PER_INCH);
-        newBleftTarget = robot.bleft.getCurrentPosition() + (int)(frightBleftInches * COUNTS_PER_INCH);
-        newBrightTarget = robot.bright.getCurrentPosition() + (int)(fleftBrightInches * COUNTS_PER_INCH);
-
-        robot.fright.setTargetPosition(newFrightTarget);
-        robot.fleft.setTargetPosition(newFleftTarget);
-        robot.bright.setTargetPosition(newBrightTarget);
-        robot.bleft.setTargetPosition(newBleftTarget);
-
-        // Turn On RUN_TO_POSITION
-        robot.fright.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        robot.fleft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        robot.bright.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        robot.bleft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-
-        // reset the timeout time and start motion.
-        runtime.reset();
-        robot.fright.setPower(Math.abs(speed));
-        robot.fleft.setPower(Math.abs(speed));
-        robot.bright.setPower(Math.abs(speed));
-        robot.bleft.setPower(Math.abs(speed));
-    }
-
-    public boolean checkEncoderDone() {
-        return !(robot.fright.isBusy() || robot.fleft.isBusy() || robot.bright.isBusy() || robot.bleft.isBusy());
-    }
-
-    public void encoderComplete(){
-        robot.fright.setPower(0);
-        robot.fleft.setPower(0);
-        robot.bright.setPower(0);
-        robot.bleft.setPower(0);
-
-        // Turn off RUN_TO_POSITION
-        robot.fright.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        robot.fleft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        robot.bright.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-        robot.bleft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
-    }
-
-
-    private void resetEncoder() {
-        position = robot.fleft.getCurrentPosition();
-    }
-
-    private double getPosition() {
-        return robot.fleft.getCurrentPosition() - position;
-    }
-
-    public void drive(double leftPower, double rightPower) {
-        robot.bleft.setPower(leftPower);
-        robot.bright.setPower(rightPower);
-        robot.fleft.setPower(leftPower);
-        robot.fright.setPower(rightPower);
-    }
-
-    private void resetAngle() {
-        lastAngles = robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
-
-        globalAngle = 0;
-    }
-
-    /**
-     * Get current cumulative angle rotation from last reset.
-     *
-     * @return Angle in degrees. + = left, - = right.
-     */
-    private double getAngle() {
-        // We experimentally determined the Z axis is the axis we want to use for heading angle.
-        // We have to process the angle because the imu works in euler angles so the Z axis is
-        // returned as 0 to +180 or 0 to -180 rolling back to -179 or +179 when rotation passes
-        // 180 degrees. We detect this transition and track the total cumulative angle of rotation.
-
-        Orientation angles = robot.imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
-
-        double deltaAngle = angles.firstAngle - lastAngles.firstAngle;
-
-        if (deltaAngle < -180)
-            deltaAngle += 360;
-        else if (deltaAngle > 180)
-            deltaAngle -= 360;
-
-        globalAngle += deltaAngle;
-
-        lastAngles = angles;
-
-        return globalAngle;
-    }
-
-    /**
-     * See if we are moving in a straight line and if not return a power correction value.
-     *
-     * @return Power adjustment, + is adjust left - is adjust right.
-     */
-    private double checkDirection() {
-        // The gain value determines how sensitive the correction is to direction changes.
-        // You will have to experiment with your robot to get small smooth direction changes
-        // to stay on a straight line.
-        double correction, angle, gain = .10;
-
-        angle = getAngle();
-
-        if (angle == 0)
-            correction = 0;             // no adjustment.
-        else
-            correction = -angle;        // reverse sign of angle for correction.
-
-        correction = correction * gain;
-
-        return correction;
-    }
-
-    /**
-     * Rotate left or right the number of degrees. Does not support turning more than 180 degrees.
-     *
-     * @param degrees Degrees to turn, + is left - is right
-     */
-    public void rotate(int degrees, double power) {
-        double leftPower, rightPower;
-
-        // restart imu movement tracking.
-        // resetAngle();
-
-        // getAngle() returns + when rotating counter clockwise (left) and - when rotating
-        // clockwise (right).
-
-        // slow as we get closer
-        if (Math.abs(getAngle() - degrees) < 0.5) {
-            power = power * 0.0;
-        }
-        else if (Math.abs(getAngle() - degrees) < 5) {
-            power = power * 0.25;
-        }
-        else if (Math.abs(getAngle() - degrees) < 10) {
-            power = power * 0.5;
-        }
-
-        if (degrees < 0) {   // turn right.
-            leftPower = -power;
-            rightPower = power;
-        } else if (degrees > 0) {   // turn left.
-            leftPower = power;
-            rightPower = -power;
-        } else return;
-
-        // set power to rotate.
-        drive(leftPower, rightPower);
-
-        // turn the motors off.
-        //drive(0, 0);
-
-        // wait for rotation to stop.
-
-        // reset angle tracking on new heading.
-        // resetAngle();
-    }
 
     private void initVuforia() {
         /*
