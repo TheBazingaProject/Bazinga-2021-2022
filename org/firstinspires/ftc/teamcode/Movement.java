0d0c9b8534f9ac9cb472f11ea46959daf359b379
Hackyzz <91227333+Hackyzz@users.noreply.github.com>
2021-11-19 19:29:29 -0600

    Update Movement.java

--- org/firstinspires/ftc/teamcode/Movement.java
+++ org/firstinspires/ftc/teamcode/Movement.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.qualcomm.robotcore.hardware.DcMotor;
+
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
@@ -9,8 +11,97 @@ public class Movement {
     double globalAngle, power = .30, correction;
     Orientation lastAngles = new Orientation();
     double position = 0;
-
+    static final double     COUNTS_PER_MOTOR_REV    = 537.6 ;
+    static final double     MAX_REV                 = 300 ;
+    static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // This is < 1.0 if geared UP
+    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double     DRIVE_SPEED             = 1;
+    static final double     TURN_SPEED              = 0.8;
     Hardwaremap robot = new Hardwaremap();
+
+    public void encoderDrive(double speed, double rightInches, double leftInches) {
+        int frightTarget;
+        int fleftTarget;
+        int brightTarget;
+        int bleftTarget;
+
+
+        // Determine new target position, and pass to motor controller
+        fleftTarget = robot.fleft.getCurrentPosition() - (int)(leftInches * COUNTS_PER_INCH);
+        frightTarget = robot.fright.getCurrentPosition() - (int)(rightInches * COUNTS_PER_INCH);
+        bleftTarget = robot.bleft.getCurrentPosition() - (int)(leftInches * COUNTS_PER_INCH);
+        brightTarget = robot.bright.getCurrentPosition() - (int)(rightInches * COUNTS_PER_INCH);
+        robot.fright.setTargetPosition(frightTarget);
+        robot.fleft.setTargetPosition(fleftTarget);
+        robot.bright.setTargetPosition(brightTarget);
+        robot.bleft.setTargetPosition(bleftTarget);
+
+        // Turn On RUN_TO_POSITION
+        robot.fright.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.fleft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.bright.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.bleft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // reset the timeout time and start motion.
+        //runtime.reset();
+        robot.fright.setPower(Math.abs(speed));
+        robot.fleft.setPower(Math.abs(speed));
+        robot.bright.setPower(Math.abs(speed));
+        robot.bleft.setPower(Math.abs(speed));
+    }
+
+    // frightBleft is positive and fleftBright is negative to strafe left
+    public void encoderStrafe(double speed,
+                              double frightBleftInches, double fleftBrightInches) {
+        int newFleftTarget;
+        int newFrightTarget;
+        int newBleftTarget;
+        int newBrightTarget;
+
+        // Determine new target position, and pass to motor controller
+        newFleftTarget = robot.fleft.getCurrentPosition() + (int)(fleftBrightInches * COUNTS_PER_INCH);
+        newFrightTarget = robot.fright.getCurrentPosition() + (int)(frightBleftInches * COUNTS_PER_INCH);
+        newBleftTarget = robot.bleft.getCurrentPosition() + (int)(frightBleftInches * COUNTS_PER_INCH);
+        newBrightTarget = robot.bright.getCurrentPosition() + (int)(fleftBrightInches * COUNTS_PER_INCH);
+
+        robot.fright.setTargetPosition(newFrightTarget);
+        robot.fleft.setTargetPosition(newFleftTarget);
+        robot.bright.setTargetPosition(newBrightTarget);
+        robot.bleft.setTargetPosition(newBleftTarget);
+
+        // Turn On RUN_TO_POSITION
+        robot.fright.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.fleft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.bright.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        robot.bleft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+        // reset the timeout time and start motion.
+        robot.fright.setPower(Math.abs(speed));
+        robot.fleft.setPower(Math.abs(speed));
+        robot.bright.setPower(Math.abs(speed));
+        robot.bleft.setPower(Math.abs(speed));
+    }
+
+    public boolean checkEncoderDone() {
+        return !(robot.fright.isBusy() || robot.fleft.isBusy() || robot.bright.isBusy() || robot.bleft.isBusy());
+    }
+
+    public void encoderComplete(){
+        robot.fright.setPower(0);
+        robot.fleft.setPower(0);
+        robot.bright.setPower(0);
+        robot.bleft.setPower(0);
+
+        // Turn off RUN_TO_POSITION
+        robot.fright.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.fleft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.bright.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.bleft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+
+
     public void resetEncoder() {
         position = robot.fleft.getCurrentPosition();
     }
@@ -99,20 +190,18 @@ public class Movement {
         // slow as we get closer
         if (Math.abs(getAngle() - degrees) < 0.5) {
             power = power * 0.0;
-        }
-        else if (Math.abs(getAngle() - degrees) < 5) {
+        } else if (Math.abs(getAngle() - degrees) < 5) {
             power = power * 0.25;
-        }
-        else if (Math.abs(getAngle() - degrees) < 10) {
+        } else if (Math.abs(getAngle() - degrees) < 10) {
             power = power * 0.5;
         }
 
         if (degrees < 0) {   // turn right.
-            leftPower = power;
-            rightPower = -power;
-        } else if (degrees > 0) {   // turn left.
             leftPower = -power;
             rightPower = power;
+        } else if (degrees > 0) {   // turn left.
+            leftPower = power;
+            rightPower = -power;
         } else return;
 
         // set power to rotate.
