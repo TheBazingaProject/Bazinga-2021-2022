a1f6a76b32daba7cc627909959c748d488228f8e
powerfulminon <77811977+powerfulminon@users.noreply.github.com>
2022-01-15 13:07:45 -0600

    Update AutoRedFarWebcam.java

--- java/org/firstinspires/ftc/teamcode/Autonomous/AutoRedFarWebcam.java
+++ java/org/firstinspires/ftc/teamcode/Autonomous/AutoRedFarWebcam.java
@@ -61,7 +61,7 @@ public class AutoRedFarWebcam extends OpMode {
     static final double     COUNTS_PER_MOTOR_REV    = 537.6 ;
     static final double     MAX_REV                 = 300 ;
     static final double     DRIVE_GEAR_REDUCTION    = 1.0 ;     // This is < 1.0 if geared UP
-    static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
+    static final double     WHEEL_DIAMETER_INCHES   = 5.0 ;     // For figuring circumference
     static final double     LIFT_WHEEL_DIAMETER_IN  = 1.0 ;
     static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
             (WHEEL_DIAMETER_INCHES * 3.1415);
